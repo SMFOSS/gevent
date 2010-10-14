58fac52c8e4e9cbce4536c4deb33e802554dd18c
Ralf Schmitt <ralf@systemexit.de>
2010-10-14 02:53:35 +0200

    use apipkg.py.

--- gevent/__init__.py
+++ gevent/__init__.py
@@ -9,45 +9,42 @@ See http://www.gevent.org/ for the documentation.
 version_info = (0, 14, 0)
 __version__ = '0.14.0dev'
 
-__all__ = ['Greenlet',
-           'GreenletExit',
-           'spawn',
-           'spawn_later',
-           'spawn_link',
-           'spawn_link_value',
-           'spawn_link_exception',
-           'spawn_raw',
-           'joinall',
-           'killall',
-           'Timeout',
-           'with_timeout',
-           'getcurrent',
-           'sleep',
-           'kill',
-           'signal',
-           'fork',
-           'shutdown',
-           'core',
-           'reinit']
-
-
 import sys
 if sys.platform == 'win32':
     __import__('socket')  # trigger WSAStartup call
 del sys
 
 
-from gevent import core
-from gevent.core import reinit
-from gevent.greenlet import Greenlet, joinall, killall
-spawn = Greenlet.spawn
-spawn_later = Greenlet.spawn_later
-spawn_link = Greenlet.spawn_link
-spawn_link_value = Greenlet.spawn_link_value
-spawn_link_exception = Greenlet.spawn_link_exception
-from gevent.timeout import Timeout, with_timeout
-from gevent.hub import getcurrent, GreenletExit, spawn_raw, sleep, kill, signal, shutdown
-try:
-    from gevent.hub import fork
-except ImportError:
-    __all__.remove('fork')
+from gevent.apipkg import initpkg
+initpkg(
+    __name__,
+    dict(
+        reinit = "gevent.core:reinit",
+
+        Greenlet = "gevent.greenlet:Greenlet",
+        joinall = "gevent.greenlet:joinall",
+        killall = "gevent.greenlet:killall",
+
+        spawn = "gevent.greenlet:Greenlet.spawn",
+        spawn_later = "gevent.greenlet:Greenlet.spawn_later",
+        spawn_link = "gevent.greenlet:Greenlet.spawn_link",
+        spawn_link_value = "gevent.greenlet:Greenlet.spawn_link_value",
+        spawn_link_exception = "gevent.greenlet:Greenlet.spawn_link_exception",
+
+        Timeout = "gevent.timeout:Timeout",
+        with_timeout = "gevent.timeout:with_timeout",
+
+        getcurrent = "gevent.hub:getcurrent",
+        GreenletExit = "gevent.hub:GreenletExit",
+        spawn_raw = "gevent.hub:spawn_raw",
+        sleep = "gevent.hub:sleep",
+        kill = "gevent.hub:kill",
+        signal = "gevent.hub:signal",
+        shutdown = "gevent.hub:shutdown",
+
+        fork = "gevent.hub:fork",
+
+        hub = "gevent.hub",
+        core = "gevent.core"),
+
+    dict(version_info=version_info))
